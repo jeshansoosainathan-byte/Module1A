3308bd90e047da5771ca296b9a8244eff76add58
AsuranAnnan <159507084+AsuranAnnan@users.noreply.github.com>
2026-01-13 17:40:41 -0500

    echoes input

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Echo!");
+Console.WriteLine("Type something!");
+string echo = Console.ReadLine();
+ 
+
+for (int i = 0; i < 3; i++)
+{
+    Console.Write(echo + " ");
+}
+ 
+
